dd6c6ccef2e1892718ee3adf18b23171a2a42e87
Gibran González <146513259+GibranNahumGonzalezSantamaria@users.noreply.github.com>
2026-06-24 20:13:10 -0600

    flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,50 @@
 {
-  description = "A very basic flake";
+  description = "Configuración multi-máquina centralizada (Estructura Plana)";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      
+      # 1. Tu PC Principal (Desktop)
+      NixOS = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix   # Archivo base suelto en la raíz
+          ./NixOS.nix           # Gimp, Kolourpaint, etc.
+          ./Virtualization.nix  # QEMU/KVM
+        ];
+      };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      # 2. La de tu hermana (IdeaCentre)
+      IdeaCentreHost = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./IdeaCentre.nix      # Autologin de tu hermana
+        ];
+      };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      # 3. La de tu mamá (IdeaPad)
+      IdeaPadHost = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./IdeaPad.nix         # Autologin de tu mamá + fix TPM
+        ];
+      };
 
+      # 4. La de tu prima (Pavilion)
+      PavilionHost = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./Pavilion.nix        # Spotify + Firewall de tu prima
+        ];
+      };
+
+    };
   };
 }
