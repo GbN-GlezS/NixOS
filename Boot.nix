42b8a0778f598c37b43af6263c2b9b2a5dec001c
Gibran González <146513259+GibranNahumGonzalezSantamaria@users.noreply.github.com>
2026-06-24 23:17:41 -0600

    Plymouth

--- Boot.nix
+++ Boot.nix
@@ -1,25 +1,24 @@
-{ config, pkgs, lib, ... }:
+# https://wiki.nixos.org/wiki/Plymouth
 
-{
-# Silent boot and native graphical animation
-    boot = {
+{ pkgs, ... }: {
+  boot = {
     plymouth = {
-        enable = true;
+      enable = true;
     };
 
     # Enable "Silent boot"
     consoleLogLevel = 0;
     initrd.verbose = false;
-
+    
     kernelParams = [
-        "quiet"
-        "rd.udev.log_level=3"
-        "rd.systemd.show_status=auto"
+      "quiet"
+      "rd.udev.log_level=3"
+      "rd.systemd.show_status=auto"
     ];
 
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
     # It will just not appear on screen unless a key is pressed
     loader.timeout = 0;
-    };
+  };
 }
