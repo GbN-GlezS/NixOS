a4559e5f9af444995b49e4c87b9b8fb3d6138493
Gibran González <146513259+GibranNahumGonzalezSantamaria@users.noreply.github.com>
2026-07-17 00:09:59 -0600

    Update Idea.nix

--- Idea.nix
+++ Idea.nix
@@ -6,14 +6,14 @@
     layout = "latam";
     variant = "";
   };
-  
-# ### Inicio de Sesión Automático (SDDM + KDE Plasma) ###
+
+  # Inicio de Sesión Automático (SDDM + KDE Plasma)
   services.displayManager.autoLogin = {
     enable = true;
     user = "nixos";
   };
 
-# ### A start job is running for /dev/tpm* ###
+  # A start job is running for /dev/tpm*
   systemd.tpm2.enable = false;
   boot.initrd.systemd.tpm2.enable = false;
 }
