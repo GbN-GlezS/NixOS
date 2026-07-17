905c915d84c05cb0e5be90889186775d1e586d5e
Gibran González <146513259+GibranNahumGonzalezSantamaria@users.noreply.github.com>
2026-07-17 00:11:35 -0600

    Format OnlyOffice.nix for consistency

--- OnlyOffice.nix
+++ OnlyOffice.nix
@@ -3,20 +3,21 @@
 { pkgs, lib, ... }:
 
 {
-# Office suite that combines text, spreadsheet and presentation editors allowing to create, view and edit local documents.
-    environment.systemPackages = with pkgs; [
-        onlyoffice-desktopeditors
-    ];
+  # Office suite that combines text, spreadsheet and presentation editors allowing to create, view and edit local documents.
+  environment.systemPackages = with pkgs; [
+    onlyoffice-desktopeditors
+  ];
 
-# Allow installation of unfree corefonts & vista-fonts packages
-    nixpkgs.config.allowUnfreePredicate = pkg:
+  # Allow installation of unfree corefonts & vista-fonts packages
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
     builtins.elem (lib.getName pkg) [
-            "corefonts"
-            "vista-fonts"
-        ];
-
-    fonts.packages = with pkgs; [
-        corefonts
-        vista-fonts
+      "corefonts"
+      "vista-fonts"
     ];
+
+  fonts.packages = with pkgs; [
+    corefonts
+    vista-fonts
+  ];
 }
