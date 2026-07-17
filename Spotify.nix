a7d45c80b6c94c115963cc82a9f86bf063d1f82a
Gibran González <146513259+GibranNahumGonzalezSantamaria@users.noreply.github.com>
2026-07-17 00:15:13 -0600

    Update Spotify.nix

--- Spotify.nix
+++ Spotify.nix
@@ -3,12 +3,12 @@
 { pkgs, ... }:
 
 {
-# Play music from the Spotify music service.
-    environment.systemPackages = with pkgs; [
-        spotify
-    ];
+  # Play music from the Spotify music service.
+  environment.systemPackages = with pkgs; [
+    spotify
+  ];
 
-# Local discovery
-    networking.firewall.allowedTCPPorts = [ 57621 ];
-    networking.firewall.allowedUDPPorts = [ 5353 ];
+  # Local discovery
+  networking.firewall.allowedTCPPorts = [ 57621 ];
+  networking.firewall.allowedUDPPorts = [ 5353 ];
 }
