1dfd1c900af89819203d27472e23b92634ecc48a
Gibran González <146513259+GibranNahumGonzalezSantamaria@users.noreply.github.com>
2026-07-17 01:06:39 -0600

    Integrate home-manager into flake.nix
    Refactor flake.nix to integrate home-manager for user configurations and simplify system definitions.

--- flake.nix
+++ flake.nix
@@ -1,68 +1,112 @@
 {
-  description = "Configuración multi-máquina centralizada (Estructura Plana)";
+  description = "A very basic flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations = {
-      
-      # 1. Tu PC Principal (Desktop)
-      NixOS = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix   # Base de tu S.O.
-          ./Plymouth.nix            # Silent boot y animación Plymouth para ti
-          ./Common.nix          # Brave, Papirus, Bibata
-          ./OnlyOffice.nix      # OnlyOffice + fuentes Microsoft para tu productividad
-          ./Spotify.nix         # Spotify + Firewall local abierto para ti
-          ./NixOS.nix           # Tus herramientas (Gimp, Kolourpaint, Colemak)
-          ./Virtualization.nix  # Entorno QEMU/KVM
-          ./VSCode.nix          # Code editor developed by Microsoft
-          ({ networking.hostName = "NixOS"; })
-        ];
-      };
-
-      # 2. La de tu hermana (IdeaCentre)
-      IdeaCentre = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          ./Plymouth.nix            # Animación fluida de arranque para ella
-          ./Common.nix          # Entorno común básico
-          ./OnlyOffice.nix      # OnlyOffice para sus tareas académicas
-          ./Idea.nix            # Autologin + Fix de teclado Latam y TPM unificado
-          ({ networking.hostName = "IdeaCentre"; })
-        ];
-      };
-
-      # 3. La de tu mamá (IdeaPad)
-      IdeaPad = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          ./Plymouth.nix            # Animación fluida de arranque para ella
-          ./Common.nix
-          ./OnlyOffice.nix      # OnlyOffice para sus documentos
-          ./Idea.nix            # Autologin + Fix de teclado Latam y TPM unificado
-          ({ networking.hostName = "IdeaPad"; })
-        ];
-      };
-
-      # 4. La de tu prima (Pavilion)
-      Pavilion = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          ./Plymouth.nix            # Animación fluida de arranque para ella
-          ./Common.nix
-          ./Spotify.nix         # Tu prima solo ocupa Spotify con su Firewall abierto
-          ./Pavilion.nix        # Exclusiones de paquetes locales
-          ({ networking.hostName = "Pavilion"; })
-        ];
-      };
-
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+
+        NixOS = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            ./Plymouth.nix
+            ./Common.nix
+            ./OnlyOffice.nix
+            ./Spotify.nix
+            ./NixOS.nix
+            ./Virtualization.nix
+            ./VSCode.nix
+            ({ networking.hostName = "NixOS"; })
+
+            home-manager.nixosModules.default
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.nixos = ./Home-NixOS.nix;
+              };
+            }
+          ];
+        };
+
+        IdeaCentre = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            ./Plymouth.nix
+            ./Common.nix
+            ./OnlyOffice.nix
+            ./Idea.nix
+            ({ networking.hostName = "IdeaCentre"; })
+
+            home-manager.nixosModules.default
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.nixos = ./Home-IdeaCentre.nix;
+              };
+            }
+          ];
+        };
+
+        IdeaPad = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            ./Plymouth.nix
+            ./Common.nix
+            ./OnlyOffice.nix
+            ./Idea.nix
+            ({ networking.hostName = "IdeaPad"; })
+
+            home-manager.nixosModules.default
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.nixos = ./Home-IdeaPad.nix;
+              };
+            }
+          ];
+        };
+
+        Pavilion = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            ./Plymouth.nix
+            ./Common.nix
+            ./Spotify.nix
+            ./Pavilion.nix
+            ({ networking.hostName = "Pavilion"; })
+
+            home-manager.nixosModules.default
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.nixos = ./Home-Pavilion.nix;
+              };
+            }
+          ];
+        };
+
+      };
+    };
 }
