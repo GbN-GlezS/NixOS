2db77dadbbde6b4e12697f7e28be42718e076e95
Gibran González <146513259+GibranNahumGonzalezSantamaria@users.noreply.github.com>
2026-07-17 00:08:22 -0600

    Refactor comments and adjust package exclusions

--- Common.nix
+++ Common.nix
@@ -1,22 +1,27 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
-# ### Which X11 packages to exclude from the default environment ###
+  # ### Which X11 packages to exclude from the default environment ###
   services.xserver.excludePackages = with pkgs; [ xterm ];
 
-# ### List of default packages to exclude from the configuration ###
+  # ### List of default packages to exclude from the configuration ###
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    discover
+    #discover
     khelpcenter
     okular
     qrca
   ];
 
   environment.systemPackages = with pkgs; [
-# ### Navegador Web ###
+    # ### Navegador Web ###
     brave
 
-# ### Personalización KDE ###
+    # ### Personalización KDE ###
     papirus-icon-theme
     bibata-cursors
   ];
