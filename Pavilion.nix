5cc2f8b4bdae93c753f312dd8426b4e90b8b9d3e
Gibran González <146513259+GibranNahumGonzalezSantamaria@users.noreply.github.com>
2026-07-17 02:30:43 -0600

    Update Pavilion.nix

--- Pavilion.nix
+++ Pavilion.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 
 {
-  # Configure keymap in X11
+  # Configure keymap in X11.
   services.xserver.xkb = {
-    layout = "latam";
-    variant = "";
+    layout = "latam"; # X keyboard layout, or multiple keyboard layouts separated by commas.
+    variant = ""; # X keyboard variant.
   };
 
-  # List of default packages to exclude from the configuration
+  # List of default packages to exclude from the configuration.
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    elisa
+    elisa # Simple music player aiming to provide a nice experience for its users.
   ];
 }
