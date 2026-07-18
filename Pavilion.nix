916cc3a5d1dafd1951cb0056d6c0475b59045ef0
Gibran González <146513259+GibranNahumGonzalezSantamaria@users.noreply.github.com>
2026-07-18 12:39:46 -0600

    Update Pavilion.nix

--- Pavilion.nix
+++ Pavilion.nix
@@ -11,4 +11,9 @@
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     elisa # Simple music player aiming to provide a nice experience for its users.
   ];
+
+  # The set of packages that appear in /run/current-system/sw.
+  environment.systemPackages = with pkgs; [
+    kdePackages.kamoso # Application to take pictures and videos with your webcam.
+  ];
 }
