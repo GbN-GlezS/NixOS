f874bd0d65b20fdb77b0ad7eac7b3888cb9c0691
Gibran González <146513259+GibranNahumGonzalezSantamaria@users.noreply.github.com>
2026-07-19 11:47:35 -0600

    Update and rename Home-Pavilion.nix to Home-Manager/Pavilion.nix

---
+++ Home-Manager/Pavilion.nix
@@ -0,0 +1,180 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  papirus-icon-theme-custom = pkgs.papirus-icon-theme.overrideAttrs (oldAttrs: {
+    dontFixup = true;
+    postInstall = (oldAttrs.postInstall or "") + ''
+      export XDG_DATA_HOME=$out/share
+      ${pkgs.papirus-folders}/bin/papirus-folders -o -C pink -t Papirus-Light
+    '';
+  });
+in
+
+{
+  home.stateVersion = "25.05";
+
+  home.packages = with pkgs; [
+    bibata-cursors # Material Based Cursor Theme.
+    inter # Typeface specially designed for user interfaces.
+    jetbrains-mono # Typeface made for developers.
+    papirus-icon-theme-custom # Pixel perfect icon theme for Linux.
+    papirus-folders # Tool to change papirus icon theme color.
+  ];
+
+  home.pointerCursor = {
+    enable = true;
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
+
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "Papirus-Light";
+      package = papirus-icon-theme-custom;
+    };
+    font = {
+      name = "Inter";
+      size = 10;
+      package = pkgs.inter;
+    };
+  };
+
+  programs.plasma = {
+    enable = true;
+    overrideConfig = true;
+
+    configFile."kdeglobals"."General"."AccentColor" = "233,58,154";
+    configFile."kdeglobals"."General"."LastUsedCustomAccentColor" = "233,58,154";
+    configFile."kdeglobals"."KDE"."widgetStyle" = "Breeze";
+    configFile."kdeglobals"."Sounds"."Theme" = "freedesktop";
+
+    fonts = {
+      general = {
+        family = "Inter";
+        pointSize = 10;
+      };
+      fixedWidth = {
+        family = "JetBrains Mono";
+        pointSize = 10;
+      };
+      small = {
+        family = "Inter";
+        pointSize = 8;
+      };
+      toolbar = {
+        family = "Inter";
+        pointSize = 10;
+      };
+      menu = {
+        family = "Inter";
+        pointSize = 10;
+      };
+      windowTitle = {
+        family = "Inter";
+        pointSize = 10;
+      };
+    };
+
+    workspace = {
+      colorScheme = "BreezeLight";
+      theme = "default";
+      iconTheme = "Papirus-Light";
+
+      cursor = {
+        theme = "Bibata-Modern-Classic";
+        size = 24;
+      };
+
+      windowDecorations = {
+        library = "org.kde.breeze";
+        theme = "Breeze";
+      };
+
+      splashScreen = {
+        theme = "None";
+      };
+    };
+
+    panels = [
+      {
+        location = "bottom";
+        alignment = "center";
+        lengthMode = "fill";
+        hiding = "dodgewindows";
+        opacity = "adaptive";
+        floating = true;
+        height = 35;
+
+        widgets = [
+          "org.kde.plasma.kickoff"
+          "org.kde.plasma.icontasks"
+          "org.kde.plasma.panelspacer"
+          "org.kde.plasma.systemtray"
+          "org.kde.plasma.digitalclock"
+        ];
+      }
+    ];
+  };
+
+  xdg.desktopEntries = {
+    "brave-whatsapp" = {
+      name = "WhatsApp";
+      exec = "brave --app=https://web.whatsapp.com/";
+      icon = "whatsapp";
+      terminal = false;
+      categories = [
+        "Network"
+        "InstantMessaging"
+      ];
+    };
+    "brave-gemini" = {
+      name = "Gemini";
+      exec = "brave --app=https://gemini.google.com/";
+      icon = "${./Icons/Gemini.svg}";
+      terminal = false;
+      categories = [
+        "Network"
+        "Utility"
+      ];
+    };
+    "brave-chatgpt" = {
+      name = "ChatGPT";
+      exec = "brave --app=https://chatgpt.com/";
+      icon = "${./Icons/ChatGPT-Light.svg}";
+      terminal = false;
+      categories = [
+        "Network"
+        "Utility"
+      ];
+    };
+    "brave-canva" = {
+      name = "Canva";
+      exec = "brave --app=https://www.canva.com/";
+      icon = "${./Icons/Canva.svg}";
+      terminal = false;
+      categories = [
+        "Network"
+        "Graphics"
+      ];
+    };
+    "brave-classroom" = {
+      name = "Google Classroom";
+      exec = "brave --app=https://classroom.google.com/";
+      icon = "${./Icons/Classroom.svg}";
+      terminal = false;
+      categories = [
+        "Network"
+        "Education"
+      ];
+    };
+  };
+}
