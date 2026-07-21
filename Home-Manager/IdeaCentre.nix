3694a020a1cf5e35884477b2a41df1c4890c23c6
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-07-21 17:57:48 -0600

    Remove desktop entries for Canva and Classroom from IdeaCentre.nix

--- Home-Manager/IdeaCentre.nix
+++ Home-Manager/IdeaCentre.nix
@@ -1,26 +1,4 @@
 { ... }:
 
 {
-  xdg.desktopEntries = {
-    "brave-canva" = {
-      name = "Canva";
-      exec = "brave --app=https://www.canva.com/";
-      icon = "${./Icons/Canva.svg}";
-      terminal = false;
-      categories = [
-        "Graphics"
-        "Network"
-      ];
-    };
-    "brave-classroom" = {
-      name = "Classroom";
-      exec = "brave --app=https://classroom.google.com/";
-      icon = "${./Icons/Classroom.svg}";
-      terminal = false;
-      categories = [
-        "Education"
-        "Network"
-      ];
-    };
-  };
 }
