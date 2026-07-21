f697a9d9d36c6b675b8da137d35441fd73fbf9a7
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-07-21 17:19:25 -0600

    Refactor flake.nix: update boot language and keymap file paths to use relative references

--- flake.nix
+++ flake.nix
@@ -34,8 +34,8 @@
             ./Hosts/NixOS.nix
             ./Common.nix
             ./Boot/Plymouth.nix
-            "./Boot/Language (en-US).nix"
-            "./Boot/Keymap (us-colemak).nix"
+            (./. + "/Boot/Language (en-US).nix")
+            (./. + "/Boot/Keymap (us-colemak).nix")
             ./Packages/OnlyOffice.nix
             ./Packages/Spotify.nix
             ./Packages/VirtManager.nix
@@ -68,8 +68,8 @@
             ./configuration.nix
             ./Hosts/Idea.nix
             ./Boot/Plymouth.nix
-            "./Boot/Language (es-MX).nix"
-            "./Boot/Keymap (latam).nix"
+            (./. + "/Boot/Language (es-MX).nix")
+            (./. + "/Boot/Keymap (latam).nix")
             ./Common.nix
             ./Packages/OnlyOffice.nix
             ({ networking.hostName = "IdeaCentre"; })
@@ -93,17 +93,14 @@
           ];
         };
 
-
-        
-
         IdeaPad = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
             ./configuration.nix
             ./Hosts/Idea.nix
             ./Boot/Plymouth.nix
-            "./Boot/Language (es-MX).nix"
-            "./Boot/Keymap (latam).nix"
+            (./. + "/Boot/Language (es-MX).nix")
+            (./. + "/Boot/Keymap (latam).nix")
             ./Common.nix
             ./Packages/OnlyOffice.nix
             ({ networking.hostName = "IdeaPad"; })
@@ -133,8 +130,8 @@
             ./configuration.nix
             ./Hosts/Pavilion.nix
             ./Boot/Plymouth.nix
-            "./Boot/Language (es-MX).nix"
-            "./Boot/Keymap (latam).nix"
+            (./. + "/Boot/Language (es-MX).nix")
+            (./. + "/Boot/Keymap (latam).nix")
             ./Common.nix
             ./Packages/Spotify.nix
             ./Packages/OnlyOffice.nix
