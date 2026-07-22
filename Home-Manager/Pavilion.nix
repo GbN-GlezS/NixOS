81fd6ff34a04b9253a875e9efd274543b3074592
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-07-22 15:55:59 -0600

    Add overrideConfig to plasma settings in IdeaCentre, IdeaPad, and Pavilion for improved configuration management

--- Home-Manager/Pavilion.nix
+++ Home-Manager/Pavilion.nix
@@ -3,6 +3,7 @@
 {
   programs.plasma = {
     enable = true;
+    overrideConfig = true;
 
     # Desactivar Baloo (indexador de archivos) declarativamente
     configFile."baloorc"."Basic Settings"."Indexing-Enabled" = false;
