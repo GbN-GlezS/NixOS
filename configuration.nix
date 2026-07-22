2c15f7421e89d8dd0bf078ad0888cf6035d32213
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-07-21 18:24:57 -0600

    Comment out unused package installations in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -85,10 +85,10 @@
       "networkmanager"
       "wheel"
     ];
-    packages = with pkgs; [
-      #  kdePackages.kate
-      #  thunderbird
-    ];
+    # packages = with pkgs; [
+    #   kdePackages.kate
+    #   thunderbird
+    # ];
   };
 
   # Install firefox.
@@ -99,10 +99,10 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  # ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
