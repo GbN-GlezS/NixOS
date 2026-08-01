0bf124235439d1c9b5b599206a2c8a6f42fba8da
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-01 03:16:45 -0600

    Update stateVersion to "26.05" in Common.nix and configuration.nix

--- Home-Manager/Common.nix
+++ Home-Manager/Common.nix
@@ -22,7 +22,7 @@ let
 in
 
 {
-  home.stateVersion = 26.05; # It is occasionally necessary for Home Manager to change configuration defaults in a way that is incompatible with stateful data.
+  home.stateVersion = "26.05"; # It is occasionally necessary for Home Manager to change configuration defaults in a way that is incompatible with stateful data.
 
   # The set of packages to appear in the user environment.
   home.packages = with pkgs; [
