d6334b0592c10fd3d735c18919263feb5d3a9fca
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-01 03:55:06 -0600

    Refactor PhotoGIMP.nix to use consistent variable naming and update comments for clarity

--- Home-Manager/PhotoGIMP.nix
+++ Home-Manager/PhotoGIMP.nix
@@ -1,22 +1,24 @@
-{ pkgs, lib, ... }:
+#https://github.com/Diolinux/Photogimp
+
+{ pkgs, ... }:
 
 let
   # Definimos la descarga del relase oficial de PhotoGIMP
-  photoGimpSrc = pkgs.fetchzip {
+  PhotoGIMP = pkgs.fetchzip {
     url = "https://github.com/Diolinux/PhotoGIMP/releases/download/3.0/PhotoGIMP-linux.zip";
-    # Hash SHA256 para garantizar la integridad reproducible de Nix
-    hash = lib.fakeHash;
+    hash = "sha256-g7JNSr6LczV0uHvy5UjRwDwVkWTGMFRd0bW9RaBoDjM=";
   };
 in
+
 {
-  # 1. Aseguramos que GIMP esté instalado en tu sistema
+  # The set of packages to appear in the user environment.
   home.packages = [
     pkgs.gimp # GNU Image Manipulation Program.
   ];
 
-  # 2. Copiamos/Viculamos la configuración de PhotoGIMP automáticamente a ~/.config
+  # Attribute set of files to link into the user home.
   home.file.".config/GIMP/3.0" = {
-    source = "${photoGimpSrc}/.config/GIMP/3.0";
-    recursive = true;
+    source = "${PhotoGIMP}/.config/GIMP/3.0"; # Path of the source file or directory.
+    recursive = true; # If the file source is a directory, then this option determines whether the directory should be recursively linked to the target location.
   };
 }
