4c7c135a75551d553893c0a09c023ff880b795f7
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-02 16:34:52 -0600

    Refactor Hyprland configuration: update package handling and clean up comments

--- Home-Manager/Hyprland.nix
+++ Home-Manager/Hyprland.nix
@@ -1,5 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   home.stateVersion = "26.05"; # It is occasionally necessary for Home Manager to change configuration defaults in a way that is incompatible with stateful data.
+
+  home.packages = with pkgs; [
+    kitty
+    fuzzel
+    waybar
+  ];
 }
--- System/Hyprland.nix
+++ System/Hyprland.nix
@@ -1,40 +1,28 @@
 { pkgs, ... }:
 
 {
-  # 1. Módulo del sistema de Hyprland
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-
-    # Habilita la integración con UWSM (recomendado para gestionar la sesión con systemd)
     withUWSM = true;
   };
 
-  # 2. Display Manager (Pantalla de inicio de sesión gráfica)
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
   };
 
-  # 3. Variables de entorno globales para Wayland
   environment.sessionVariables = {
-    # Fuerza a aplicaciones Electron/Chromium (VSCode, Spotify, Brave, etc.) a usar Wayland nativo
     NIXOS_OZONE_WL = "1";
   };
 
-  # 4. Portales XDG para la captura/compartición de pantalla
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-hyprland
+
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
     ];
   };
 
-  # 5. Paquetes base del sistema
-  environment.systemPackages = with pkgs; [
-    kitty # Terminal requerida por defecto
-    fuzzel # Lanzador de aplicaciones ligero
-    waybar # Barra de estado superior
-    dconf # Requerido para almacenar configuraciones GTK/System
-  ];
+  programs.dconf.enable = true;
 }
