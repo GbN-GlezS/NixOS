aee6a6504dcebbe19703bdb43eae2cf2349e777a
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-01 19:10:16 -0600

    Refactor host configuration generation for improved maintainability and consistency

--- flake.nix
+++ flake.nix
@@ -21,198 +21,108 @@
       plasma-manager,
       ...
     }:
+    let
+      # Function to generate a host configuration with default fallback values
+      mkHost =
+        {
+          hostName,
+          sysLocale ? "es_MX.UTF-8",
+          kbdLayout ? "latam",
+          ThemeColor ? "pink",
+          IconVariant ? "Light",
+          CursorVariant ? "Classic",
+          AccentColor ? "233,58,154",
+          LookAndFeel ? "",
+          extraSystemModules ? [ ],
+          extraHomeModules ? [ ],
+        }:
+        nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
 
+          specialArgs = {
+            inherit sysLocale kbdLayout;
+            kbdVariant = "";
+          };
+
+          modules = [
+            ./Hosts/${hostName}/configuration.nix
+            ./Hosts/Common.nix
+            ./Boot/Plymouth.nix
+            ./Packages/OnlyOffice.nix
+            ({ networking.hostName = hostName; })
+          ]
+          ++ extraSystemModules
+          ++ [
+
+            home-manager.nixosModules.default
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                backupFileExtension = "backup";
+                overwriteBackup = true;
+
+                extraSpecialArgs = {
+                  inherit
+                    ThemeColor
+                    IconVariant
+                    CursorVariant
+                    AccentColor
+                    LookAndFeel
+                    ;
+                  CursorSize = 16;
+                  FontSize = 10;
+                };
+
+                sharedModules = [ plasma-manager.homeModules.plasma-manager ];
+                users.nixos = {
+                  imports = [
+                    ./Home-Manager/Hosts/${hostName}.nix
+                    ./Home-Manager/Hosts/Common.nix
+                  ]
+                  ++ extraHomeModules;
+                };
+              };
+            }
+          ];
+        };
+
+    in
     {
       nixosConfigurations = {
 
-        IdeaCentre = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
+        IdeaCentre = mkHost {
+          hostName = "IdeaCentre";
+        };
 
-          specialArgs = {
-            sysLocale = "es_MX.UTF-8";
-            kbdLayout = "latam";
-            kbdVariant = "";
-          };
+        IdeaPad = mkHost {
+          hostName = "IdeaPad";
+          IconVariant = "Dark";
+          CursorVariant = "Ice";
+          LookAndFeel = "dark";
+        };
 
-          modules = [
-            ./Hosts/IdeaCentre/configuration.nix
-            ./Hosts/Common.nix
-            ./Boot/Plymouth.nix
-            ./Packages/OnlyOffice.nix
-            ({ networking.hostName = "IdeaCentre"; })
+        Pavilion = mkHost {
+          hostName = "Pavilion";
+          extraSystemModules = [ ./Packages/Spotify.nix ];
+        };
 
-            home-manager.nixosModules.default
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                backupFileExtension = "backup";
-                overwriteBackup = true;
-
-                extraSpecialArgs = {
-                  ThemeColor = "pink";
-                  IconVariant = "Light";
-                  CursorVariant = "Classic";
-                  CursorSize = 16;
-                  FontSize = 10;
-                  AccentColor = "233,58,154";
-                  LookAndFeel = "";
-                };
-
-                sharedModules = [ plasma-manager.homeModules.plasma-manager ];
-                users.nixos = {
-                  imports = [
-                    ./Home-Manager/Hosts/IdeaCentre.nix
-                    ./Home-Manager/Hosts/Common.nix
-                  ];
-                };
-              };
-            }
+        ThinkPad = mkHost {
+          hostName = "ThinkPad";
+          sysLocale = "en_US.UTF-8";
+          kbdLayout = "us";
+          ThemeColor = "blue";
+          IconVariant = "Dark";
+          CursorVariant = "Ice";
+          AccentColor = "61,174,233";
+          LookAndFeel = "dark";
+          extraSystemModules = [ ./Packages/Spotify.nix ];
+          extraHomeModules = [
+            ./Home-Manager/Packages/VSCode.nix
+            ./Home-Manager/Packages/PhotoGIMP.nix
           ];
         };
 
-        IdeaPad = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-
-          specialArgs = {
-            sysLocale = "es_MX.UTF-8";
-            kbdLayout = "latam";
-            kbdVariant = "";
-          };
-
-          modules = [
-            ./Hosts/IdeaPad/configuration.nix
-            ./Hosts/Common.nix
-            ./Boot/Plymouth.nix
-            ./Packages/OnlyOffice.nix
-            ({ networking.hostName = "IdeaPad"; })
-
-            home-manager.nixosModules.default
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                backupFileExtension = "backup";
-                overwriteBackup = true;
-
-                extraSpecialArgs = {
-                  ThemeColor = "pink";
-                  IconVariant = "Dark";
-                  CursorVariant = "Ice";
-                  CursorSize = 16;
-                  FontSize = 10;
-                  AccentColor = "233,58,154";
-                  LookAndFeel = "dark";
-                };
-
-                sharedModules = [ plasma-manager.homeModules.plasma-manager ];
-                users.nixos = {
-                  imports = [
-                    ./Home-Manager/Hosts/IdeaPad.nix
-                    ./Home-Manager/Hosts/Common.nix
-                  ];
-                };
-              };
-            }
-          ];
-        };
-
-        Pavilion = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-
-          specialArgs = {
-            sysLocale = "es_MX.UTF-8";
-            kbdLayout = "latam";
-            kbdVariant = "";
-          };
-
-          modules = [
-            ./Hosts/Pavilion/configuration.nix
-            ./Hosts/Common.nix
-            ./Boot/Plymouth.nix
-            ./Packages/Spotify.nix
-            ./Packages/OnlyOffice.nix
-            ({ networking.hostName = "Pavilion"; })
-
-            home-manager.nixosModules.default
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                backupFileExtension = "backup";
-                overwriteBackup = true;
-
-                extraSpecialArgs = {
-                  ThemeColor = "pink";
-                  IconVariant = "Light";
-                  CursorVariant = "Classic";
-                  CursorSize = 16;
-                  FontSize = 10;
-                  AccentColor = "233,58,154";
-                  LookAndFeel = "";
-                };
-
-                sharedModules = [ plasma-manager.homeModules.plasma-manager ];
-                users.nixos = {
-                  imports = [
-                    ./Home-Manager/Hosts/Pavilion.nix
-                    ./Home-Manager/Hosts/Common.nix
-                  ];
-                };
-              };
-            }
-          ];
-        };
-
-        ThinkPad = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-
-          specialArgs = {
-            sysLocale = "en_US.UTF-8";
-            kbdLayout = "us";
-            kbdVariant = "";
-          };
-
-          modules = [
-            ./Hosts/ThinkPad/configuration.nix
-            ./Hosts/Common.nix
-            ./Boot/Plymouth.nix
-            ./Packages/OnlyOffice.nix
-            ./Packages/Spotify.nix
-            #./Packages/VirtManager.nix
-            ({ networking.hostName = "ThinkPad"; })
-
-            home-manager.nixosModules.default
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                backupFileExtension = "backup";
-                overwriteBackup = true;
-
-                extraSpecialArgs = {
-                  ThemeColor = "blue";
-                  IconVariant = "Dark";
-                  CursorVariant = "Ice";
-                  CursorSize = 16;
-                  FontSize = 10;
-                  AccentColor = "61,174,233";
-                  LookAndFeel = "dark";
-                };
-
-                sharedModules = [ plasma-manager.homeModules.plasma-manager ];
-                users.nixos = {
-                  imports = [
-                    ./Home-Manager/Hosts/ThinkPad.nix
-                    ./Home-Manager/Hosts/Common.nix
-                    ./Home-Manager/Packages/VSCode.nix
-                    ./Home-Manager/Packages/PhotoGIMP.nix
-                  ];
-                };
-              };
-            }
-          ];
-        };
       };
     };
 }
