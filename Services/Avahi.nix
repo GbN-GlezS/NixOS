c2f93c347a280d17345b726d3b70a8757968f5ae
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-01 21:03:09 -0600

    Add comments to Avahi service configuration for clarity

--- Services/Avahi.nix
+++ Services/Avahi.nix
@@ -2,15 +2,15 @@
 
 {
   services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    nssmdns6 = true;
-    openFirewall = true;
+    enable = true; # Whether to run the Avahi daemon, which allows Avahi clients to use Avahi's service discovery facilities and also allows the local machine to advertise its presence and services (through the mDNS responder implemented by avahi-daemon).
+    nssmdns4 = true; # Whether to enable the mDNS NSS (Name Service Switch) plug-in for IPv4.
+    nssmdns6 = true; # Whether to enable the mDNS NSS (Name Service Switch) plug-in for IPv6.
+    openFirewall = true; # Whether to open the firewall for UDP port 5353.
 
     publish = {
-      enable = true;
-      addresses = true;
-      workstation = true;
+      enable = true; # Whether to allow publishing in general.
+      addresses = true; # Whether to register mDNS address records for all local IP addresses.
+      workstation = true; # Whether to register a service of type "_workstation._tcp" on the local LAN.
     };
   };
 
