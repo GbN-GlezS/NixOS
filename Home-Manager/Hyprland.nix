34d8984303fdeac85ca8ba4dfa96a4acf28afe3e
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-02 17:33:22 -0600

    Add wayland.windowManager.hyprland configuration with keybindings and monitor settings

--- Home-Manager/Hyprland.nix
+++ Home-Manager/Hyprland.nix
@@ -13,4 +13,22 @@
     enable = true;
     systemd.enable = true;
   };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+
+    settings = {
+      "$mod" = "SUPER";
+
+      bind = [
+        "$mod, Return, exec, kitty"
+        "$mod, D, exec, fuzzel"
+        "$mod, Q, killactive"
+      ];
+
+      monitor = [
+        ",preferred,auto,1"
+      ];
+    };
+  };
 }
