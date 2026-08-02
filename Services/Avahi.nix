a14e83bf5c2cbc9ca6670165f9b3d1702c2c5582
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-01 21:03:59 -0600

    Remove redundant comment about Avahi waiting for network and IP readiness

--- Services/Avahi.nix
+++ Services/Avahi.nix
@@ -14,7 +14,6 @@
     };
   };
 
-  # Fuerza a Avahi a esperar a que la red y la IP estén completamente listas
   systemd.services.avahi-daemon = {
     wants = [ "network-online.target" ];
     after = [ "network-online.target" ];
