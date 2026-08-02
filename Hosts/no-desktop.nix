61e36c2807e1960467e0cd72d4fe9059f18c5a42
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-02 16:42:48 -0600

    Add experimental Nix features: enable nix-command and flakes

--- Hosts/no-desktop.nix
+++ Hosts/no-desktop.nix
@@ -5,6 +5,11 @@
 { ... }:
 
 {
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
