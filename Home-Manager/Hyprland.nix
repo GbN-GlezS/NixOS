8f90a8b001e98d7a82ef2057b386ceecf8967a56
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-02 22:51:21 -0600

    Update Hyprland configuration: restructure settings and remove redundant comments

--- Home-Manager/Hyprland.nix
+++ Home-Manager/Hyprland.nix
@@ -3,7 +3,34 @@
 {
   home.username = "nixos";
   home.homeDirectory = "/home/nixos";
-  home.stateVersion = "26.05"; # Mantenido según tu requerimiento
+  home.stateVersion = "26.05";
+
+  # 1. Desactivar systemd en Home Manager para no pisotear UWSM
+  wayland.windowManager.hyprland = {
+    enable = true;
+    systemd.enable = false;
+
+    # Sincronizar paquetes con el módulo de NixOS
+    package = null;
+    portalPackage = null;
+
+    # Forzar hyprlang tradicional
+    configType = "hyprlang";
+
+    settings = {
+      "$mainMod" = "SUPER";
+
+      # Binds explícitos
+      bind = [
+        "$mainMod, Q, exec, kitty"
+        "$mainMod, E, exec, thunar"
+        "$mainMod, R, exec, rofi -show drun"
+        "$mainMod, SPACE, exec, rofi -show drun"
+        "$mainMod, C, killactive,"
+        "$mainMod, M, exit,"
+      ];
+    };
+  };
 
   # Aplicaciones del usuario
   home.packages = with pkgs; [
@@ -11,38 +38,15 @@
     rofi
   ];
 
-  # Terminal Kitty
   programs.kitty = {
     enable = true;
   };
 
-  # Barra de estado Waybar
   programs.waybar = {
     enable = true;
     systemd = {
       enable = true;
       targets = [ "graphical-session.target" ];
-    };
-  };
-
-  # Configuración de Hyprland
-  wayland.windowManager.hyprland = {
-    enable = true;
-
-    # OBLIGATORIO para stateVersion >= 26.05 si usas sintaxis de hyprland.conf ($ variables)
-    configType = "hyprlang";
-
-    settings = {
-      "$mainMod" = "SUPER";
-
-      bind = [
-        "$mainMod, Q, exec, ${pkgs.kitty}/bin/kitty"
-        "$mainMod, E, exec, ${pkgs.thunar}/bin/thunar"
-        "$mainMod, R, exec, ${pkgs.rofi}/bin/rofi -show drun"
-        "$mainMod, SPACE, exec, ${pkgs.rofi}/bin/rofi -show drun"
-        "$mainMod, C, killactive,"
-        "$mainMod, M, exit,"
-      ];
     };
   };
 }
