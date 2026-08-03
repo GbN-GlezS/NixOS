8a12e25af038da460fd58809e9fc245cdc7520f5
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-02 22:26:05 -0600

    Refactor Hyprland configuration: streamline package settings and clean up keybindings

--- Home-Manager/Hyprland.nix
+++ Home-Manager/Hyprland.nix
@@ -3,17 +3,13 @@
 {
   home.username = "nixos";
   home.homeDirectory = "/home/nixos";
-  home.stateVersion = "26.05";
+  home.stateVersion = "26.05"; # Se mantiene intacto según requerimiento
 
   # Aplicaciones del usuario
   home.packages = with pkgs; [
     thunar
+    rofi
   ];
-
-  # Menú de aplicaciones (Rofi)
-  programs.rofi = {
-    enable = true;
-  };
 
   # Terminal Kitty
   programs.kitty = {
@@ -33,19 +29,18 @@
   wayland.windowManager.hyprland = {
     enable = true;
 
-    # Forzar el formato hyprlang tradicional para ignorar la generación de Lua
-    configType = "hyprlang";
+    # Al ser stateVersion >= 26.05, estructuramos settings de forma limpia
+    settings = {
+      "$mainMod" = "SUPER";
 
-    extraConfig = ''
-      $mainMod = SUPER
-
-      # Atajos de teclado
-      bind = $mainMod, Q, exec, kitty
-      bind = $mainMod, E, exec, thunar
-      bind = $mainMod, R, exec, rofi -show drun
-      bind = $mainMod, SPACE, exec, rofi -show drun
-      bind = $mainMod, C, killactive,
-      bind = $mainMod, M, exit,
-    '';
+      bind = [
+        "$mainMod, Q, exec, ${pkgs.kitty}/bin/kitty"
+        "$mainMod, E, exec, ${pkgs.thunar}/bin/thunar"
+        "$mainMod, R, exec, ${pkgs.rofi}/bin/rofi -show drun"
+        "$mainMod, SPACE, exec, ${pkgs.rofi}/bin/rofi -show drun"
+        "$mainMod, C, killactive,"
+        "$mainMod, M, exit,"
+      ];
+    };
   };
 }
