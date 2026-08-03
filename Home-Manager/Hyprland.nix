d4c12d88c2b732c0ac8eef64215f3af2b17eca4a
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-02 21:53:17 -0600

    Refactor Hyprland configuration: remove unused settings and enhance Waybar integration with systemd

--- Home-Manager/Hyprland.nix
+++ Home-Manager/Hyprland.nix
@@ -7,34 +7,29 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-
-    # Forzar la sintaxis hyprlang tradicional
     configType = "hyprlang";
 
     settings = {
       "$mainMod" = "SUPER";
-
-      exec-once = [
-        "waybar"
-      ];
 
       bind = [
         "$mainMod, Q, exec, kitty"
         "$mainMod, C, killactive,"
         "$mainMod, M, exit,"
       ];
+    };
+  };
 
-      windowrule = [
-        "border_size 2, kitty"
-      ];
+  # Configuración e integración de Waybar vía systemd
+  programs.waybar = {
+    enable = true;
+    systemd = {
+      enable = true;
+      target = "graphical-session.target";
     };
   };
 
   programs.kitty = {
-    enable = true;
-  };
-
-  programs.waybar = {
     enable = true;
   };
 }
