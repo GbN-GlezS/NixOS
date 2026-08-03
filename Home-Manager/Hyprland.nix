8b736206f9ef969df6792c7d3d7c4a88344caee9
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-02 22:53:25 -0600

    Update Hyprland configuration: reorganize settings and enhance UWSM integration

--- Home-Manager/Hyprland.nix
+++ Home-Manager/Hyprland.nix
@@ -1,36 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   home.username = "nixos";
   home.homeDirectory = "/home/nixos";
   home.stateVersion = "26.05";
-
-  # 1. Desactivar systemd en Home Manager para no pisotear UWSM
-  wayland.windowManager.hyprland = {
-    enable = true;
-    systemd.enable = false;
-
-    # Sincronizar paquetes con el módulo de NixOS
-    package = null;
-    portalPackage = null;
-
-    # Forzar hyprlang tradicional
-    configType = "hyprlang";
-
-    settings = {
-      "$mainMod" = "SUPER";
-
-      # Binds explícitos
-      bind = [
-        "$mainMod, Q, exec, kitty"
-        "$mainMod, E, exec, thunar"
-        "$mainMod, R, exec, rofi -show drun"
-        "$mainMod, SPACE, exec, rofi -show drun"
-        "$mainMod, C, killactive,"
-        "$mainMod, M, exit,"
-      ];
-    };
-  };
 
   # Aplicaciones del usuario
   home.packages = with pkgs; [
@@ -38,15 +11,49 @@
     rofi
   ];
 
+  # Exportar variables de entorno de Home Manager a UWSM
+  xdg.configFile."uwsm/env".source =
+    "${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh";
+
+  # Terminal Kitty
   programs.kitty = {
     enable = true;
   };
 
+  # Barra de estado Waybar
   programs.waybar = {
     enable = true;
     systemd = {
       enable = true;
       targets = [ "graphical-session.target" ];
+    };
+  };
+
+  # Configuración de Hyprland alineada con UWSM
+  wayland.windowManager.hyprland = {
+    enable = true;
+
+    # 1. Usar el paquete y portal de NixOS UWSM
+    package = null;
+    portalPackage = null;
+
+    # 2. OBLIGATORIO: Desactivar systemd en HM para no generar conflicto con UWSM
+    systemd.enable = false;
+
+    # 3. Formato de configuración plano
+    configType = "hyprlang";
+
+    settings = {
+      "$mainMod" = "SUPER";
+
+      bind = [
+        "$mainMod, Q, exec, ${pkgs.kitty}/bin/kitty"
+        "$mainMod, E, exec, ${pkgs.thunar}/bin/thunar"
+        "$mainMod, R, exec, ${pkgs.rofi}/bin/rofi -show drun"
+        "$mainMod, SPACE, exec, ${pkgs.rofi}/bin/rofi -show drun"
+        "$mainMod, C, killactive,"
+        "$mainMod, M, exit,"
+      ];
     };
   };
 }
