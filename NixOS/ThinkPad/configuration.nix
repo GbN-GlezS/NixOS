bca42b103d3b5ee87a3bb9ffd0242ef179ed5b2e
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-03 02:17:13 -0600

    Update NixOS configuration: set system.stateVersion to "26.11"

--- NixOS/ThinkPad/configuration.nix
+++ NixOS/ThinkPad/configuration.nix
@@ -120,5 +120,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "26.11"; # Did you read the comment?
-
 }
