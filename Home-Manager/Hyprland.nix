b4b3f788102e81d3c41a765468b62f1df42edfa3
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-02 21:32:04 -0600

    Add home.stateVersion to Hyprland configuration for compatibility with Home Manager

--- Home-Manager/Hyprland.nix
+++ Home-Manager/Hyprland.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 
 {
+  home.stateVersion = "26.05"; # It is occasionally necessary for Home Manager to change configuration defaults in a way that is incompatible with stateful data.
+
   # Habilitar Hyprland
   wayland.windowManager.hyprland = {
     enable = true;
