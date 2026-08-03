35dd33361867316746ecdc1d8b47ba7769fa625c
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-02 22:02:04 -0600

    Update Hyprland configuration: change stateVersion to 26.05, add Rofi settings, and refine keybindings

--- Home-Manager/Hyprland.nix
+++ Home-Manager/Hyprland.nix
@@ -3,13 +3,18 @@
 {
   home.username = "nixos";
   home.homeDirectory = "/home/nixos";
-  home.stateVersion = "24.05";
+  home.stateVersion = "26.05";
 
-  # Instalación del explorador de archivos (Thunar)
+  # Instalación de paquetes de usuario
   home.packages = with pkgs; [
     xfce.thunar
-    brave
   ];
+
+  # Configuración de Rofi (Menú de aplicaciones para Wayland)
+  programs.rofi = {
+    enable = true;
+    package = pkgs.rofi-wayland;
+  };
 
   # Configuración de Hyprland
   wayland.windowManager.hyprland = {
@@ -21,7 +26,9 @@
 
       bind = [
         "$mainMod, Q, exec, kitty"
-        "$mainMod, E, exec, thunar" # Atajo para abrir el explorador de archivos
+        "$mainMod, E, exec, thunar"
+        "$mainMod, R, exec, rofi -show drun" # Menú de aplicaciones con SUPER + R
+        "$mainMod, SPACE, exec, rofi -show drun" # Opción alternativa con SUPER + Espacio
         "$mainMod, C, killactive,"
         "$mainMod, M, exit,"
       ];
