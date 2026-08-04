ea94d88ff7667e5ee097d44596d5087ca23c4a6f
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-04 11:31:50 -0600

    Refactor GTK configuration by removing unused theme, icon, and cursor settings

--- Home-Manager/Hosts/GTK.nix
+++ Home-Manager/Hosts/GTK.nix
@@ -1,10 +1,5 @@
 {
   pkgs,
-  ThemeColor ? "pink",
-  IconVariant ? "Light",
-  CursorVariant ? "Classic",
-  CursorSize ? 16,
-  FontSize ? 10,
   OrchisTheme ? "pink", # default | purple | pink | red | orange | yellow | green | teal | grey
   OrchisColor ? "dark", # standard | light | dark
   OrchisSize ? "standard", # standard | compact
@@ -16,7 +11,6 @@
 }:
 
 let
-  # Creamos el paquete del tema con las variaciones elegidas usando overrideAttrs
   orchis-theme-custom = pkgs.orchis-theme.overrideAttrs (oldAttrs: {
     postInstall = (oldAttrs.postInstall or "") + ''
       ${pkgs.bash}/bin/bash ./install.sh \
@@ -29,66 +23,23 @@ let
         ) "--tweaks ${pkgs.lib.concatStringsSep " " OrchisTweaks}"}
     '';
   });
-
-  # Reutilizamos la personalización de Papirus-Folders idéntica a tu Common.nix
-  Papirus-Icon-Theme-Custom = pkgs.papirus-icon-theme.overrideAttrs (oldAttrs: {
-    dontFixup = true;
-    postInstall = (oldAttrs.postInstall or "") + ''
-      export XDG_DATA_HOME="$out/share"
-      ${pkgs.papirus-folders}/bin/papirus-folders -o -C ${ThemeColor} -t Papirus
-      ${pkgs.papirus-folders}/bin/papirus-folders -o -C ${ThemeColor} -t Papirus-Dark
-      ${pkgs.papirus-folders}/bin/papirus-folders -o -C ${ThemeColor} -t Papirus-Light
-    '';
-  });
 in
 {
-  home.stateVersion = "26.05";
-
-  # Paquetes requeridos para el funcionamiento e íconos/cursores
+  # Add Orchis theme and Murrine engine to packages
   home.packages = with pkgs; [
-    bibata-cursors
-    inter
-    jetbrains-mono
-    papirus-folders
-    Papirus-Icon-Theme-Custom
     orchis-theme-custom
-    gtk-engine-murrine # Dependencia necesaria para temas GTK2/3 basados en Murrine
+    gtk-engine-murrine
   ];
 
-  # Configuración del Cursor
-  home.pointerCursor = {
-    enable = true;
-    gtk.enable = true;
-    x11.enable = true;
-    name = "Bibata-Modern-" + CursorVariant;
-    package = pkgs.bibata-cursors;
-    size = CursorSize;
-  };
-
-  # Configuración principal de GTK
+  # GTK Theme configuration
   gtk = {
     enable = true;
 
-    # Tema de aplicaciones GTK (Orchis)
     theme = {
       name = "Orchis-" + OrchisTheme + "-" + OrchisColor;
       package = orchis-theme-custom;
     };
 
-    # Tema de Íconos
-    iconTheme = {
-      name = "Papirus-" + IconVariant;
-      package = Papirus-Icon-Theme-Custom;
-    };
-
-    # Tipografía
-    font = {
-      name = "Inter";
-      size = FontSize;
-      package = pkgs.inter;
-    };
-
-    # Ajustes GTK3 / GTK4 extras
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = if OrchisColor == "dark" then 1 else 0;
     };
@@ -98,13 +49,11 @@ in
     };
   };
 
-  # Configuración dconf para sincronizar la preferencia global de esquema de color en Wayland / GNOME / Flatpak
+  # Sync dark theme setting across Desktop Environments / Flatpaks
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = if OrchisColor == "dark" then "prefer-dark" else "default";
       gtk-theme = "Orchis-" + OrchisTheme + "-" + OrchisColor;
-      icon-theme = "Papirus-" + IconVariant;
-      cursor-theme = "Bibata-Modern-" + CursorVariant;
     };
   };
 }
