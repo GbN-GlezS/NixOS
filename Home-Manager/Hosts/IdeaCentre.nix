dd18f3708423a5c3aedc605bb3edcec6008753e9
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-04 11:18:40 -0600

    Remove Plasma configuration and home.stateVersion from IdeaCentre and IdeaPad host files

--- Home-Manager/Hosts/IdeaCentre.nix
+++ Home-Manager/Hosts/IdeaCentre.nix
@@ -1,11 +1,4 @@
 { ... }:
 
 {
-  programs.plasma = {
-    enable = true;
-    overrideConfig = true;
-
-    # Desactivar Baloo (indexador de archivos) declarativamente
-    configFile."baloorc"."Basic Settings"."Indexing-Enabled" = false;
-  };
 }
--- Home-Manager/Hosts/IdeaPad.nix
+++ Home-Manager/Hosts/IdeaPad.nix
@@ -1,13 +1,4 @@
 { ... }:
 
 {
-  home.stateVersion = "26.05"; # It is occasionally necessary for Home Manager to change configuration defaults in a way that is incompatible with stateful data.
-
-  programs.plasma = {
-    enable = true;
-    overrideConfig = true;
-
-    # Desactivar Baloo (indexador de archivos) declarativamente
-    configFile."baloorc"."Basic Settings"."Indexing-Enabled" = false;
-  };
 }
