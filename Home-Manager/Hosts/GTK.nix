d09ae6200ad46261e72436e7f8372654dc64b406
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-04 11:42:23 -0600

    Refactor GTK configuration by simplifying theme parameters and hardcoding values

--- Home-Manager/Hosts/GTK.nix
+++ Home-Manager/Hosts/GTK.nix
@@ -1,59 +1,42 @@
 {
   pkgs,
-  OrchisTheme ? "pink", # default | purple | pink | red | orange | yellow | green | teal | grey
-  OrchisColor ? "dark", # standard | light | dark
-  OrchisSize ? "standard", # standard | compact
-  OrchisTweaks ? [
-    "solid"
-    "primary"
-  ], # e.g. [ "solid" "compact" "black" "primary" "macos" "submenu" "nord" "dracula" ]
   ...
 }:
 
 let
   orchis-theme-custom = pkgs.orchis-theme.overrideAttrs (oldAttrs: {
     postInstall = (oldAttrs.postInstall or "") + ''
-      ${pkgs.bash}/bin/bash ./install.sh \
-        -d $out/share/themes \
-        -t ${OrchisTheme} \
-        -c ${OrchisColor} \
-        -s ${OrchisSize} \
-        ${pkgs.lib.optionalString (
-          OrchisTweaks != [ ]
-        ) "--tweaks ${pkgs.lib.concatStringsSep " " OrchisTweaks}"}
+      ${pkgs.bash}/bin/bash ./install.sh -d $out/share/themes -t pink -c dark --tweaks solid primary
     '';
   });
 in
 {
-  # Add Orchis theme and Murrine engine to packages
-  home.packages = with pkgs; [
+  home.packages = [
     orchis-theme-custom
-    gtk-engine-murrine
+    pkgs.gtk-engine-murrine
   ];
 
-  # GTK Theme configuration
   gtk = {
     enable = true;
 
     theme = {
-      name = "Orchis-" + OrchisTheme + "-" + OrchisColor;
+      name = "Orchis-pink-dark";
       package = orchis-theme-custom;
     };
 
     gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = if OrchisColor == "dark" then 1 else 0;
+      gtk-application-prefer-dark-theme = 1;
     };
 
     gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = if OrchisColor == "dark" then 1 else 0;
+      gtk-application-prefer-dark-theme = 1;
     };
   };
 
-  # Sync dark theme setting across Desktop Environments / Flatpaks
   dconf.settings = {
     "org/gnome/desktop/interface" = {
-      color-scheme = if OrchisColor == "dark" then "prefer-dark" else "default";
-      gtk-theme = "Orchis-" + OrchisTheme + "-" + OrchisColor;
+      color-scheme = "prefer-dark";
+      gtk-theme = "Orchis-pink-dark";
     };
   };
 }
