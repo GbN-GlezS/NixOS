e0a3ea847fe6963a9deab68632f90d90b93c9f89
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-04 15:37:39 -0600

    Remove commented wallpaper image path from stylix configuration for clarity

--- Hosts/ThinkPad/configuration.nix
+++ Hosts/ThinkPad/configuration.nix
@@ -8,9 +8,7 @@
 
   stylix = {
     enable = true;
-    # image = /home/nixos/wallpaper.png; # Ruta a tu fondo de pantalla
-    # Opcional: Si prefieres un esquema de colores específico en lugar de generarlo del fondo
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    targets.regreet.enable = false;
   };
 
   # List of default packages to exclude from the configuration.
