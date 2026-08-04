295fa660f96cc4f6bb4a8e84af4ac8c237c712ca
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-04 13:15:02 -0600

    Refactor Xfce configuration to streamline xfconf settings and define panel properties via XML

--- Home-Manager/Hosts/Xfce.nix
+++ Home-Manager/Hosts/Xfce.nix
@@ -1,56 +1,44 @@
 { pkgs, ... }:
 
 {
+  # Configuraciones simples de Xfconf (temas y preferencias básicas)
   xfconf.settings = {
-    # Para el tema GTK y bordes
     xfwm4 = {
       "/general/theme" = "Orchis-Pink-Dark";
     };
-
     xsettings = {
       "/Net/ThemeName" = "Orchis-Pink-Dark";
     };
-
-    # Si usas arreglos en xfce4-panel, asegúrate de que los enteros se traten como int de Xfconf:
-    xfce4-panel = {
-      "/panels" = [
-        {
-          type = "int";
-          value = 1;
-        }
-      ];
-      "/panels/panel-1/plugin-ids" = [
-        {
-          type = "int";
-          value = 1;
-        }
-        {
-          type = "int";
-          value = 2;
-        }
-        {
-          type = "int";
-          value = 3;
-        }
-        {
-          type = "int";
-          value = 4;
-        }
-      ];
-      "/panels/panel-1/position" = "p=10;x=0;y=0";
-      "/panels/panel-1/length" = {
-        type = "int";
-        value = 100;
-      };
-      "/panels/panel-1/size" = {
-        type = "int";
-        value = 36;
-      };
-
-      "/plugins/plugin-1" = "whiskermenu";
-      "/plugins/plugin-2" = "tasklist";
-      "/plugins/plugin-3" = "separator";
-      "/plugins/plugin-4" = "clock";
-    };
   };
+
+  # Definición declarativa del panel vía XML
+  xdg.configFile."xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml".text = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <channel name="xfce4-panel" version="1.0">
+      <property name="panels" type="array">
+        <value type="int" value="1"/>
+        <property name="panel-1" type="empty">
+          <property name="position" type="string" value="p=10;x=0;y=0"/>
+          <property name="length" type="uint" value="100"/>
+          <property name="position-locked" type="bool" value="true"/>
+          <property name="size" type="uint" value="36"/>
+          <property name="plugin-ids" type="array">
+            <value type="int" value="1"/>
+            <value type="int" value="2"/>
+            <value type="int" value="3"/>
+            <value type="int" value="4"/>
+          </property>
+        </property>
+      </property>
+      <property name="plugins" type="empty">
+        <property name="plugin-1" type="string" value="whiskermenu"/>
+        <property name="plugin-2" type="string" value="tasklist"/>
+        <property name="plugin-3" type="string" value="separator">
+          <property name="expand" type="bool" value="true"/>
+          <property name="style" type="uint" value="0"/>
+        </property>
+        <property name="plugin-4" type="string" value="clock"/>
+      </property>
+    </channel>
+  '';
 }
