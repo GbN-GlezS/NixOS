71b585f561d6b244b830a1df20b539dcfa3458f6
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-04 12:43:07 -0600

    Update Xfce configuration to set dark theme for window manager and GTK

--- Home-Manager/Hosts/Xfce.nix
+++ Home-Manager/Hosts/Xfce.nix
@@ -1,16 +1,18 @@
 {
   OrchisColor,
-  IconVariant ? "Light",
   ...
 }:
 
 {
-  # Habilita la gestión de configuraciones de Xfce mediante Home Manager
   xfconf.settings = {
-    # Canal para la configuración del gestor de ventanas (Xfwm4)
+    # Tema para los bordes de ventana (Xfwm4)
     xfwm4 = {
-      # Establece el tema de bordes de ventana para que coincida con el tema GTK
-      "general/theme" = "Orchis-" + OrchisColor + "-" + IconVariant;
+      "/general/theme" = "Orchis-" + OrchisColor + "-Dark";
+    };
+
+    # Tema GTK global
+    xsettings = {
+      "/Net/ThemeName" = "Orchis-" + OrchisColor + "-Dark";
     };
   };
 }
