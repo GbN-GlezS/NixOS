954eddfc7e9a9d27c7f6a222717afd7620fd9dce
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-04 14:37:17 -0600

    Comment out Hyprland configuration in flake.nix to disable its usage

--- flake.nix
+++ flake.nix
@@ -159,7 +159,7 @@
             plasma-manager.homeModules.plasma-manager
             caelestia-shell.homeManagerModules.default
             ./Home-Manager/Desktop/Plasma.nix
-            ./Home-Manager/Desktop/Hyprland.nix
+            #./Home-Manager/Desktop/Hyprland.nix
             ./Home-Manager/Desktop/caelestia-shell.nix
             ./Home-Manager/Packages/VSCode.nix
             ./Home-Manager/Packages/PhotoGIMP.nix
