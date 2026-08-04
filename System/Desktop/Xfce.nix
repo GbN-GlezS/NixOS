7b2ddad15ea7c647de56e6b10f796514fb964bf3
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-04 11:03:39 -0600

    Refactor Xfce configuration for improved clarity and structure

--- System/Desktop/Xfce.nix
+++ System/Desktop/Xfce.nix
@@ -1,25 +1,18 @@
 # https://wiki.nixos.org/wiki/Xfce
 
-{ ... }:
-
 {
   services.xserver = {
     enable = true;
 
     desktopManager = {
       xterm.enable = false;
+      xfce.enable = true;
+    };
 
-      xfce = {
-        enable = true;
-      };
+    displayManager = {
+      lightdm.enable = true;
     };
   };
 
-  services.displayManager = {
-    defaultSession = "xfce";
-
-    lightdm = {
-      enable = true;
-    };
-  };
+  services.displayManager.defaultSession = "xfce";
 }
