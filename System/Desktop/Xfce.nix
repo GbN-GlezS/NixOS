e7d90f1b18f3f17733facc2eef73849ea54e9796
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-04 13:20:46 -0600

    Refactor Xfce configuration to include additional plugins for xfce4-panel and system utilities

--- System/Desktop/Xfce.nix
+++ System/Desktop/Xfce.nix
@@ -1,5 +1,7 @@
 # https://wiki.nixos.org/wiki/Xfce
 
+{ pkgs, ... }:
+
 {
   programs.xfconf.enable = true;
 
@@ -17,4 +19,12 @@
   };
 
   services.displayManager.defaultSession = "xfce";
+
+  # Plugins adicionales para xfce4-panel y utilidades de entorno
+  environment.systemPackages = with pkgs.xfce; [
+    xfce4-whiskermenu-plugin # Menú de aplicaciones moderno
+    xfce4-pulseaudio-plugin # Control e indicador de volumen PulseAudio/PipeWire
+    xfce4-clipman-plugin # Gestor de portapapeles
+    xfce4-screenshooter # Herramienta de capturas de pantalla
+  ];
 }
