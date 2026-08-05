41ff42da359f2a56f9b7d4f5de64cfcb3b1b23e6
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-05 14:21:21 -0600

    Comment out default packages in Plasma configuration for clarity

--- System/Desktop/Plasma.nix
+++ System/Desktop/Plasma.nix
@@ -11,12 +11,12 @@
 
   # List of default packages to exclude from the configuratio
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    discover # Helps you find and install applications, games, and tools.
-    elisa # Simple music player aiming to provide a nice experience for its users.
-    kate # Advanced text editor.
-    khelpcenter # Software documentation viewer.
+    # discover # Helps you find and install applications, games, and tools.
+    # elisa # Simple music player aiming to provide a nice experience for its users.
+    # kate # Advanced text editor.
+    # khelpcenter # Software documentation viewer.
     # krunner # Framework for providing different actions given a string query.
     # plasma-browser-integration # Components necessary to integrate browsers into the Plasma Desktop.
-    qrca # QR code scanner for Plasma Mobile.
+    # qrca # QR code scanner for Plasma Mobile.
   ];
 }
