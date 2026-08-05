fd0c67964180f4cd5abbf3da36d26ffde45839dc
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-05 17:05:20 -0600

    Refactor Brave browser configuration: remove Widevine support from override settings

--- Home-Manager/Packages/Brave.nix
+++ Home-Manager/Packages/Brave.nix
@@ -5,10 +5,7 @@
 {
   # The set of packages to appear in the user environment.
   home.packages = with pkgs; [
-    (brave.override {
-      enableWideVine = true;
-      commandLineArgs = [ "--password-store=basic" ];
-    }) # Privacy-oriented browser for Desktop and Laptop computers (https://wiki.nixos.org/wiki/Chromium).
+    (brave.override { commandLineArgs = [ "--password-store=basic" ]; }) # Privacy-oriented browser for Desktop and Laptop computers.
   ];
 
   # Desktop Entries allow applications to be shown in your desktop environment's app launcher.
