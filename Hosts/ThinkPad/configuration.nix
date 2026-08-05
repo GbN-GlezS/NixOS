7493865765d4ea6c999a06de1f800f295c6f4734
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-05 15:49:02 -0600

    Refactor package exclusion comments for clarity and consistency

--- Hosts/ThinkPad/configuration.nix
+++ Hosts/ThinkPad/configuration.nix
@@ -8,15 +8,16 @@
 
   # List of default packages to exclude from the configuration.
   # environment.plasma6.excludePackages = with pkgs.kdePackages; [
-  #   okular # KDE document viewer.
+  # okular # KDE document viewer.
   # ];
 
   # The set of packages that appear in /run/current-system/sw.
-  # environment.systemPackages = with pkgs.kdePackages; [
-  #   kolourpaint # Easy-to-use paint program.
-  #   partitionmanager # Manage the disk devices, partitions and file systems on your computer.
-  #   skanpage # Utility to scan images and multi-page documents.
-  # ];
+  environment.systemPackages = with pkgs.kdePackages; [
+    kitty # Fast, feature-rich, GPU based terminal emulator.
+    # kolourpaint # Easy-to-use paint program.
+    # partitionmanager # Manage the disk devices, partitions and file systems on your computer.
+    # skanpage # Utility to scan images and multi-page documents.
+  ];
 
   # programs.kdeconnect.enable = true; # Whether to enable kdeconnect.
 
