9cd56ccd8fdeba533276d1cc841a97895ac8ecb5
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-05 16:49:01 -0600

    Add Brave browser configuration to Home Manager setup

--- flake.nix
+++ flake.nix
@@ -70,6 +70,7 @@
                     plasma-manager.homeModules.plasma-manager
                     ./Home-Manager/Desktop/Plasma.nix
                     ./Home-Manager/Hosts/Common.nix
+                    ./Home-Manager/Packages/Brave.nix
                     ./Home-Manager/Packages/DesktopEntries.nix
                     ./Home-Manager/Packages/OnlyOffice.nix
                   ]
