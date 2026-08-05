17059b823b518e3247b880d8f28d027d947cba06
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-05 15:39:38 -0600

    Uncomment okular in excluded packages for accessibility

--- System/Desktop/Plasma.nix
+++ System/Desktop/Plasma.nix
@@ -27,7 +27,7 @@
     krdp # Library and examples for creating an RDP server.
     ktexteditor # KTextEditor Framework.
     kwin-x11 # Easy to use, but flexible, X Window Manager.
-    # okular # KDE document viewer.
+    okular # KDE document viewer.
     plasma-browser-integration # Components necessary to integrate browsers into the Plasma Desktop.
     plasma-keyboard # Virtual Keyboard for Qt based desktops.
     plasma-workspace-wallpapers # Wallpapers for Plasma Workspaces.
