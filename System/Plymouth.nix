305b773da3b764bd96aa67d4f1f90c07cd198382
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-05 23:19:03 -0600

    Adjust kernel parameters for Plymouth boot splash: set udev log level to 0 and disable systemd status display

--- System/Plymouth.nix
+++ System/Plymouth.nix
@@ -14,8 +14,8 @@
     # Parameters added to the kernel command line.
     kernelParams = [
       "quiet"
-      "rd.udev.log_level=3"
-      "rd.systemd.show_status=auto"
+      "rd.udev.log_level=0"
+      "rd.systemd.show_status=no"
     ];
 
     loader.timeout = 0; # Timeout (in seconds) until loader boots the default menu item.
