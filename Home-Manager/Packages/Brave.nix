de0fdf8632fa3010befbedf089cd0c343b11889e
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-05 18:04:54 -0600

    Enhance Brave browser configuration: add additional command line arguments for performance and privacy

--- Home-Manager/Packages/Brave.nix
+++ Home-Manager/Packages/Brave.nix
@@ -5,7 +5,27 @@
 {
   # The set of packages to appear in the user environment.
   home.packages = with pkgs; [
-    (brave.override { commandLineArgs = [ "--password-store=basic" ]; }) # Privacy-oriented browser for Desktop and Laptop computers.
+    (brave.override {
+      commandLineArgs = [
+        # --- Almacenamiento y Autenticación ---
+        "--password-store=basic"
+
+        # --- Rendimiento Gráfico y Aceleración por Hardware ---
+        "--enable-features=AcceleratedVideoEncoder,VaapiVideoDecoder"
+        "--ignore-gpu-blocklist"
+        "--enable-zero-copy"
+
+        # --- Gestión de Memoria RAM y Recursos de CPU ---
+        "--enable-low-end-device-mode"
+        "--aggressive-tab-discard"
+
+        # --- Reducción de Telemetría, Privacidad y Red ---
+        "--disable-background-networking"
+        "--disable-breakpad"
+        "--disable-component-update"
+        "--no-pings"
+      ];
+    }) # Privacy-oriented browser for Desktop and Laptop computers.
   ];
 
   # Desktop Entries allow applications to be shown in your desktop environment's app launcher.
