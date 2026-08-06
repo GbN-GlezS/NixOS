8006c572d9516fd2768765ff10c6318ea595cc38
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-05 21:14:10 -0600

    Add default application associations for OnlyOffice in xdg.mimeApps

--- Home-Manager/Packages/OnlyOffice.nix
+++ Home-Manager/Packages/OnlyOffice.nix
@@ -15,4 +15,36 @@
     ".local/share/fonts/corefonts".source = "${pkgs.corefonts}/share/fonts/truetype"; # Path of the source file or directory.
     ".local/share/fonts/vista-fonts".source = "${pkgs.vista-fonts}/share/fonts/truetype"; # Path of the source file or directory.
   };
+
+  xdg.mimeApps = {
+    enable = true; # Whether to manage $XDG_CONFIG_HOME/mimeapps.list.
+
+    # Packages whose .desktop files will be used to establish default mimetype associations
+    defaultApplications = {
+      "application/pdf" = [ "onlyoffice-desktopeditors.desktop" ]; # PDF
+
+      # Word Processing Documents
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [
+        "onlyoffice-desktopeditors.desktop"
+      ]; # .docx
+      "application/msword" = [ "onlyoffice-desktopeditors.desktop" ]; # .doc
+      "application/vnd.oasis.opendocument.text" = [ "onlyoffice-desktopeditors.desktop" ]; # .odt
+      "text/plain" = [ "onlyoffice-desktopeditors.desktop" ]; # .txt
+
+      # Spreadsheets
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [
+        "onlyoffice-desktopeditors.desktop"
+      ]; # .xlsx
+      "application/vnd.ms-excel" = [ "onlyoffice-desktopeditors.desktop" ]; # .xls
+      "application/vnd.oasis.opendocument.spreadsheet" = [ "onlyoffice-desktopeditors.desktop" ]; # .ods
+      "text/csv" = [ "onlyoffice-desktopeditors.desktop" ]; # .csv
+
+      # Presentations
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation" = [
+        "onlyoffice-desktopeditors.desktop"
+      ]; # .pptx
+      "application/vnd.ms-powerpoint" = [ "onlyoffice-desktopeditors.desktop" ]; # .ppt
+      "application/vnd.oasis.opendocument.presentation" = [ "onlyoffice-desktopeditors.desktop" ]; # .odp
+    };
+  };
 }
