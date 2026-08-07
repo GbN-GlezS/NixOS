e22e89f99f4ddc1a4292cff4a92bb1b197410850
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-06 19:42:15 -0600

    Reorder kernel parameters in Plymouth configuration for improved clarity

--- System/Plymouth.nix
+++ System/Plymouth.nix
@@ -17,7 +17,8 @@
     # Parameters added to the kernel command line.
     kernelParams = [
       "quiet"
-      # "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "rd.systemd.show_status=false"
       # "systemd.show_status=false"
       # "rd.udev.log_level=3"
       # "udev.log_priority=3"
