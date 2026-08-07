716756f7be191b91a856408ca2032eac169ae55a
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-06 21:00:29 -0600

    Refactor Plymouth configuration for improved structure and clarity

--- System/Plymouth.nix
+++ System/Plymouth.nix
@@ -7,19 +7,14 @@
 
 {
   boot = {
-    plymouth = {
-      enable = true; # Whether to enable Plymouth boot splash screen.
-      theme = "breeze"; # Splash screen theme.
-    };
-
+    plymouth.enable = true; # Whether to enable Plymouth boot splash screen.
     consoleLogLevel = 3; # The kernel console loglevel.
+    loader.timeout = 0; # Timeout (in seconds) until loader boots the default menu item.
 
     initrd = {
       verbose = false; # Verbosity of the initrd.
       kernelModules = [ GPU ]; # Set of modules that are always loaded by the initrd.
     };
-
-    loader.timeout = 0; # Timeout (in seconds) until loader boots the default menu item.
 
     # Parameters added to the kernel command line.
     kernelParams = [
