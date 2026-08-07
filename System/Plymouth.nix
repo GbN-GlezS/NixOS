1e15b6f47a3da22cb102c78b52d9df432284a834
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-06 19:49:18 -0600

    Update kernel parameter for udev log level to improve boot logging

--- System/Plymouth.nix
+++ System/Plymouth.nix
@@ -17,7 +17,7 @@
     # Parameters added to the kernel command line.
     kernelParams = [
       "quiet"
-      "rd.udev.log_level=3"
+      "rd.udev.log_level=0"
       "rd.systemd.show_status=false"
       # "systemd.show_status=false"
       # "rd.udev.log_level=3"
