ac12a972533dd5a701dc88cf19b322a42e84db23
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-06 19:26:55 -0600

    Comment out verbose logging in initrd and kernel parameters in Plymouth configuration

--- System/Plymouth.nix
+++ System/Plymouth.nix
@@ -8,19 +8,19 @@
     consoleLogLevel = 0; # The kernel console loglevel.
 
     initrd = {
-      verbose = false; # Verbosity of the initrd.
+      # verbose = false; # Verbosity of the initrd.
       kernelModules = [ GPU ]; # Set of modules that are always loaded by the initrd.
     };
 
     loader.timeout = 0; # Timeout (in seconds) until loader boots the default menu item.
 
     # Parameters added to the kernel command line.
-    #    kernelParams = [
-    #     "quiet"
-    #    "rd.systemd.show_status=false"
-    #   "systemd.show_status=false"
-    #  "rd.udev.log_level=3"
+    # kernelParams = [
+    # "quiet"
+    # "rd.systemd.show_status=false"
+    # "systemd.show_status=false"
+    # "rd.udev.log_level=3"
     # "udev.log_priority=3"
-    #];
+    # ];
   };
 }
