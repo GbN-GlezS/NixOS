2ffcf764b081fd67cd712a352fdeea8acf6201e1
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-10 16:44:16 -0600

    Enhance Firefox configuration with default profile and uBlock Origin installation

--- Home-Manager/Packages/Firefox.nix
+++ Home-Manager/Packages/Firefox.nix
@@ -3,8 +3,22 @@
 {
   programs.firefox = {
     enable = true;
+
     profiles.default = {
+      id = 0;
+      name = "default";
       isDefault = true;
+    };
+
+    # Instalación de extensiones y políticas globales del navegador
+    policies = {
+      ExtensionSettings = {
+        # Configuración para instalar uBlock Origin desde Mozilla Add-ons
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
     };
   };
 }
