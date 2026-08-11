eb76f0743c6b55117813167d875b2b5aa2a01c31
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-11 16:26:07 -0600

    Fix NixPkgs URL casing for consistency in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    NixPkgs.url = "github:NixOS/NixPkgs/NixOS-Unstable";
+    NixPkgs.url = "github:NixOS/NixPkgs/nixos-unstable";
 
     NUR = {
       url = "github:Nix-Community/NUR";
