2c933194a7ea19893744a8a84680e32f66ff7f48
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-11 02:43:14 -0600

    Fix casing for NixPkgs URL in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    NixPkgs.url = "GitHub:NixOS/nixpkgs?ref=NixOS-unstable";
+    NixPkgs.url = "GitHub:NixOS/nixpkgs?ref=NixOS-Unstable";
 
     HomeManager = {
       url = "GitHub:Nix-Community/Home-Manager";
