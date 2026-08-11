7370d5bb021b696b97be4c457652e5a8854f0ddb
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-11 02:36:54 -0600

    Add configuration files for IdeaCentre, IdeaPad, Pavilion, and ThinkPad with auto-login support and hardware imports

---
+++ Hosts/ThinkPad/Configuration.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./Hardware.nix
+  ];
+
+  # List of default packages to exclude from the configuration.
+  # environment.plasma6.excludePackages = with pkgs.kdePackages; [
+  # okular # KDE document viewer.
+  # ];
+
+  # The set of packages that appear in /run/current-system/sw.
+  environment.systemPackages = with pkgs.kdePackages; [
+    # kolourpaint # Easy-to-use paint program.
+    konsole # Terminal emulator by KDE.
+    partitionmanager # Manage the disk devices, partitions and file systems on your computer.
+    # skanpage # Utility to scan images and multi-page documents.
+  ];
+
+  programs.kdeconnect.enable = true; # Whether to enable kdeconnect.
+
+  # programs.nix-ld.enable = true; # Whether to enable nix-ld.
+}
