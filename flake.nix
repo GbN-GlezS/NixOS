96e28597f4885a2d332f89839a193974d7651857
Gibran González <146513259+GbN-GlezS@users.noreply.github.com>
2026-08-10 19:59:35 -0600

    Rename Home-Manager package files with "Home-" prefix and update flake.nix

--- flake.nix
+++ flake.nix
@@ -101,8 +101,8 @@
                     ./Home-Manager/Hosts/Common.nix
 
                     # Common packages
-                    ./Home-Manager/Packages/BraveOrigin.nix
-                    ./Home-Manager/Packages/OnlyOffice.nix
+                    ./Home-Manager/Packages/Home-BraveOrigin.nix
+                    ./Home-Manager/Packages/Home-OnlyOffice.nix
                   ]
                   ++ extraHomeModules;
                 };
@@ -176,9 +176,9 @@
           };
 
           extraHomeModules = [
-            ./Home-Manager/Packages/Firefox.nix
-            ./Home-Manager/Packages/PhotoGIMP.nix
-            ./Home-Manager/Packages/VSCode.nix
+            ./Home-Manager/Packages/Home-Firefox.nix
+            ./Home-Manager/Packages/Home-PhotoGIMP.nix
+            ./Home-Manager/Packages/Home-VSCode.nix
           ];
         };
       };
