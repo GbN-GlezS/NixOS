b4444d18367b0a5fce78953350c6b567be5fdc3e
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-11 21:49:51 -0600

    Remove git from system packages and comment out OpenSSH service in Configuration.nix

--- Hosts/IdeaPad/Configuration.nix
+++ Hosts/IdeaPad/Configuration.nix
@@ -71,7 +71,6 @@
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
-    git
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -85,7 +84,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
