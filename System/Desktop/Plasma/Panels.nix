aff344e5c2db60d1f6569886ab2fc320a4dedd0c
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-14 22:49:41 -0600

    Remove unused widgets from Plasma panel configuration

--- System/Desktop/Plasma/Panels.nix
+++ System/Desktop/Plasma/Panels.nix
@@ -13,12 +13,10 @@
       # The widgets to use in the panel.
       widgets = [
         "org.kde.plasma.kickoff"
-        "org.kde.plasma.pager"
         "org.kde.plasma.icontasks"
         "org.kde.plasma.marginsseparator"
         "org.kde.plasma.systemtray"
         "org.kde.plasma.digitalclock"
-        "org.kde.plasma.showdesktop"
       ];
     }
   ];
