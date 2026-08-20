ccd070349a33b0d38cf0805ff94e312984591358
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-08-19 18:20:01 -0600

    Add PWA profiles for ChatGPT, Gemini, WhatsApp, Gmail, and YouTube in Firefox configuration

--- Packages/Firefox.nix
+++ Packages/Firefox.nix
@@ -54,6 +54,38 @@
   programs.firefoxpwa = {
     enable = true;
 
+    profiles = {
+      "01M0E8HAF9MBZMZ8K3J0W3TK6Z" = {
+        name = "PWAs";
+
+        sites = {
+          "01M0E8HBAGVCB7AMQMQG4PGAS3" = {
+            name = "ChatGPT";
+            url = "https://chatgpt.com/";
+          };
+
+          "01M0E8HBX8NF8CT74869PRNBCA" = {
+            name = "Gemini";
+            url = "https://gemini.google.com/";
+          };
+
+          "01M0E8HCFHCVZD04GYAHVR48AM" = {
+            name = "WhatsApp";
+            url = "https://web.whatsapp.com/";
+          };
+
+          "01M0E8HD1RYEKGP0X45T180TEK" = {
+            name = "Gmail";
+            url = "https://mail.google.com/";
+          };
+
+          "01M0E8HG613NRZ8H5FK9H39P72" = {
+            name = "YouTube";
+            url = "https://www.youtube.com/";
+          };
+        };
+      };
+    };
   };
 
   # Set Firefox as the system-wide default handler
